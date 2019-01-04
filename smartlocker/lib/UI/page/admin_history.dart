d2c9ed3d7b349eb78f72a8c9d0c7ed48549b4ea8
MeowKawaii <pingpong_jane@hotmail.com>
2019-01-05 02:45:43 +0700

    add drop down in history screen

--- smartlocker/lib/UI/page/admin_history.dart
+++ smartlocker/lib/UI/page/admin_history.dart
@@ -33,29 +33,113 @@ class _HistoryState extends State<History> {
 }
 //----------------------------------------------------------------------------------------
 
-class HistoryBody extends StatelessWidget {
+class HistoryBody extends StatefulWidget {
+  @override
+  HistoryBodyState createState() {
+    return new HistoryBodyState();
+  }
+}
+
+//----------------------------------------------------------------------------------------------
+class HistoryBodyState extends State<HistoryBody> {
+  List _itemFilter = ["วัน เวลา", "หมายเลขล็อกเกอร์", "หมายเลขตู้"];
+
+  List<DropdownMenuItem<String>> _dropDownMenuItems;
+  String _currentItem;
+
+  void backToAdminHome() {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => HomeAdmin(),
+        ));
+  }
+
+  @override
+  void initState() {
+    _dropDownMenuItems = getDropDownMenuItems();
+    _currentItem = _dropDownMenuItems[0].value;
+    super.initState();
+  }
+
+  List<DropdownMenuItem<String>> getDropDownMenuItems() {
+    List<DropdownMenuItem<String>> items = new List();
+    for (String listHistory in _itemFilter) {
+      items.add(new DropdownMenuItem(
+          value: listHistory, child: new Text(listHistory)));
+    }
+    return items;
+  }
+
+//----------------------------------------------------------------------
   @override
   Widget build(BuildContext context) {
+    final _backIcon = new BackIcon(backToAdminHome: backToAdminHome);
+
     return Scaffold(
       body: Center(
-        child: ListView(
-          children: <Widget>[
-            IconButton(
-              alignment: Alignment.topLeft,
-              icon: Icon(Icons.arrow_back),
-              onPressed: (){
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => HomeAdmin(),
+        child: Container(
+          padding: EdgeInsets.fromLTRB(23.00, 24.00, 37.09, 24.00),
+          child: ListView(
+            children: <Widget>[
+              //--------------------------------
+              _backIcon,
+              //--------------------------------
+              Row(
+                children: <Widget>[
+                  Text("เรียงตาม : "),
+                  DropdownButtonHideUnderline(
+                    child: Container(
+                      child: new DropdownButton(
+                        isDense: true,
+                        value: _currentItem,
+                        items: _dropDownMenuItems,
+                        onChanged: changedDropDownItem,
+                      ),
+                      padding: EdgeInsets.fromLTRB(5, 5, 5, 5),
+                      decoration: ShapeDecoration(
+                        shape: RoundedRectangleBorder(
+                          side:
+                              BorderSide(width: 1.0, style: BorderStyle.solid),
+                          borderRadius: BorderRadius.all(Radius.circular(2.0)),
+                        ),
+                      ),
+                    ),
                   )
-                );
-              },
-            ),
-            Container(),
-          ],
+                ],
+                mainAxisAlignment: MainAxisAlignment.center,
+              ),
+              //--------------------------------
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+  void changedDropDownItem(String selectedCity) {
+    setState(() {
+      _currentItem = selectedCity;
+    });
+  }
+}
+
+class BackIcon extends StatelessWidget {
+  Function backToAdminHome;
+  BackIcon({Key key, this.backToAdminHome})
+      : super(
+          key: key,
+        );
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      alignment: Alignment.topLeft,
+      icon: Icon(
+        Icons.arrow_back,
+        size: 20,
+      ),
+      onPressed: backToAdminHome,
     );
   }
 }
